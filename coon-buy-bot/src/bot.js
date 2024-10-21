516db1afc786962b44a5efe439af7a21e1e29b99
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2024-10-21 10:04:19 +0100

    Update bot.js

--- coon-buy-bot/src/bot.js
+++ coon-buy-bot/src/bot.js
@@ -270,11 +270,6 @@ let startPolling = () => {
 
                 //if the signature of the topmost tx is differnt 
 
-                if (txs[0][0].signature[0] !== InitSignature) {
-                    console.log(txs[0][0].signature[0], ' yelp')
-                    InitSignature = txs[0][0].signature[0]
-                    await redis.set("InitSignature", txs[0][0].signature[0])
-                }
 
                 let ts_id = 0
                 while (txs[ts_id][0].signature[0] !== InitSignature) {
@@ -297,6 +292,12 @@ let startPolling = () => {
                         }
 
                     }
+                    
+                if (txs[0][0].signature[0] !== InitSignature) {
+                    console.log(txs[0][0].signature[0], ' yelp')
+                    InitSignature = txs[0][0].signature[0]
+                    await redis.set("InitSignature", txs[0][0].signature[0])
+                }
 
 
                     ts_id++
